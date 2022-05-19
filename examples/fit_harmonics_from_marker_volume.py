38146d4aa2990e29996869c75da084bc380646b4
unknown <bwheelz360@gmail.com>
2022-05-19 12:54:49 +1000

    fixed #23

--- examples/fit_harmonics_from_marker_volume.py
+++ examples/fit_harmonics_from_marker_volume.py
@@ -13,9 +13,9 @@ data_loc = Path(r'C:\Users\Brendan\Downloads\MRI_distortion_QA_sample_data(2)\MR
 # ^^ update to where you put the sample data!!
 
 # distorted centroids
-distorted_volume = MarkerVolume(data_loc / 'MR' / '04 gre_trans_AP_330' / 'MR.mrk.json', verbose=False)
+distorted_volume = MarkerVolume(data_loc / 'MR' / '04 gre_trans_AP_330' / 'slicer_centroids.mrk.json', verbose=False)
 # ground truth centroids
-ground_truth_volume = MarkerVolume(data_loc / 'CT' / 'MR.mrk.json', verbose=False, r_max=300)
+ground_truth_volume = MarkerVolume(data_loc / 'CT' / 'slicer_centroids.mrk.json', verbose=False, r_max=300)
 dicom_data_loc = data_loc / 'MR' / '04 gre_trans_AP_330' / 'dicom_data.json'  # previosly saved from a MarkerVolume
 
 B0_Harmonics, G_x_Harmonics, G_y_Harmonics, G_z_Harmonics = calculate_harmonics(ground_truth_volume, distorted_volume,
