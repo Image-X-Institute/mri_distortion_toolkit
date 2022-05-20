cb369c8a0482e167bed476af78db4605dfae7e7d
unknown <bwheelz360@gmail.com>
2022-05-20 18:31:36 +1000

    brute forcing it...

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -73,17 +73,17 @@ def copy_and_overwrite(from_path, to_path):
 
 this_dir = Path(__file__).parent
 print(f'{this_dir} exists? {os.path.isdir(this_dir)}')
+print(f'this_dir / _static? {os.path.isdir(this_dir / "_static")}')
 print(f'MR_QA_report_20_05_2022.html exists? {os.path.isfile(this_dir / "_static" / "MR_QA_report_20_05_2022.html")}')
 print(f'this_dir / _build? {os.path.isdir(this_dir / "_build")}')
 print(f'this_dir / docs? {os.path.isdir(this_dir / "docs")}')
-print(f'this_dir / .docs? {os.path.isdir(this_dir / ".docs")}')
-print(f'this_dir / /.docs? {os.path.isdir(this_dir / "/.docs")}')
-print(f'this_dir / _build / html? {os.path.isdir(this_dir / "_build" / "html")}')
-print(f'this_dir / _build / html /static? {os.path.isdir(this_dir / "_build" / "html" / "_static")}')
-# shutil.copy(this_dir / '_static' / 'MR_QA_report_20_05_2022.html',
-#             this_dir / '_build' / 'html' / '_static' / 'MR_QA_report_20_05_2022.html')
-# copy_and_overwrite(this_dir / '_static' / 'plots',
-#             this_dir / '_build' / 'html' / '_static' / 'plots')
-# copy_and_overwrite(this_dir / '_static' / 'themes',
-#             this_dir / '_build' / 'html' / '_static' / 'themes')
+print(f'this_dir / .. / docs? {os.path.isdir(this_dir.parent / "docs")}')
+
+
+shutil.copy(this_dir / '_static' / 'MR_QA_report_20_05_2022.html',
+            this_dir / '_build' / 'html' / '_static' / 'MR_QA_report_20_05_2022.html')
+copy_and_overwrite(this_dir / '_static' / 'plots',
+            this_dir / '_build' / 'html' / '_static' / 'plots')
+copy_and_overwrite(this_dir / '_static' / 'themes',
+            this_dir / '_build' / 'html' / '_static' / 'themes')
 
