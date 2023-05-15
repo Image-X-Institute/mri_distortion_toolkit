1f4b3e373b44ca7d7f934d677b75057e8d72f7ee
bwheelz36 <64300337+bwheelz36@users.noreply.github.com>
2023-05-15 15:39:03 +1000

    Update __init__.py

--- mri_distortion_toolkit/__init__.py
+++ mri_distortion_toolkit/__init__.py
@@ -4,7 +4,7 @@ Please see here for detailed documentation
 https://acrf-image-x-institute.github.io/MRI_DistortionQA/index.html
 """
 
-__version__ = '0.14.1'
+__version__ = '0.14.2'
 try:
     import FreeCAD
 except ImportError:
